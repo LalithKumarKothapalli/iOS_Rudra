572656c515ad6c87909216519c8836681484c60e
Lalith Kumar Kothapalli <lkumar@arrow.com>
2014-12-07 21:01:27 -0700

    Default Files

---
+++ iOS_Rudra/iOS_RudraTests/iOS_RudraTests.m
@@ -0,0 +1,34 @@
+//
+//  iOS_RudraTests.m
+//  iOS_RudraTests
+//
+//  Created by Lalith Kothapalli Venkata Naga on 12/7/14.
+//  Copyright (c) 2014 ___STARPATHIT___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS_RudraTests : XCTestCase
+
+@end
+
+@implementation iOS_RudraTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
